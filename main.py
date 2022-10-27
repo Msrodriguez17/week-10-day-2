cad285a03cafa11be59748686400e822d257e528
Moises Rodriguez <113930437+Msrodriguez17@users.noreply.github.com>
2022-10-27 17:22:08 +0000

    dividing numbers

---
+++ main.py
@@ -0,0 +1,12 @@
+#from methods_help import method_help
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
+# from returnStuff import returnS
+
+#method_help()
+function()
+welcome("Moises")
+added = add_numbers(146, 20)
+print(added)
+num_dividedFrom_add_numbers(45, added)
+
+# returnS()
